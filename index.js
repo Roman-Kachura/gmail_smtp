29bf6d1b03508a16d7a64e0e3e7108880cba6f6a
Roman-Kachura <Roman-Kachura@users.noreply.github.com>
2022-03-25 19:52:34 +0100

    13 commit

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+require('./router/index')(app);
 
 const smtp_login = process.env.SMTP_LOGIN;
 const smtp_password = process.env.SMTP_PASSWORD;
