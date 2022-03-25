b5cce682d49f853bc41b207972b4406ffaba98e3
Roman-Kachura <Roman-Kachura@users.noreply.github.com>
2022-03-25 10:54:31 +0100

    6 commit

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 const port = process.env.PORT || 3010;
 const bodyParser = require("body-parser");
 
-const smtp_login = process.env.SMTP_LOGIN;
-const smtp_password = process.env.SMTP_PASSWORD;
-
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+const smtp_login = process.env.SMTP_LOGIN;
+const smtp_password = process.env.SMTP_PASSWORD;
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
